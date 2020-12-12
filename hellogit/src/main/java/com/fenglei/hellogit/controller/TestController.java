32fb4d48792731142e14e085ed9485a02e274bb7
fenglei <fenglei@qq.com>
2020-12-12 19:27:57 +0800

    创建分支1

--- hellogit/src/main/java/com/fenglei/hellogit/controller/TestController.java
+++ hellogit/src/main/java/com/fenglei/hellogit/controller/TestController.java
@@ -3,5 +3,6 @@ package com.fenglei.hellogit.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("版本号2");
+        System.out.println("创建分支");
     }
 }
